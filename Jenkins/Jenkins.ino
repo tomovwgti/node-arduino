437a9e504137da56bcbbd0f14d6ed9879d67ab77
tomo watanabe <t.watanabe@freebit.net>
2013-07-16 17:14:49 +0900

    modify status.sh

--- Jenkins/Jenkins.ino
+++ Jenkins/Jenkins.ino
@@ -4,17 +4,17 @@
   フルカラーLEDの色を変える
  */
 
-#define LED_RED    9
-#define LED_BLUE  10
-#define LED_GREEN 11
+#define LED_RED    3
+#define LED_BLUE   6
+#define LED_GREEN  5
 
 void setup() {
   pinMode(LED_RED, OUTPUT);     // 赤LED接続のピン(9番)をアナログ出力に設定
   pinMode(LED_BLUE, OUTPUT);     // 青LED接続のピン(10番)をアナログ出力に設定
   pinMode(LED_GREEN, OUTPUT);     // 緑LED接続のピン(11番)をアナログ出力に設定
-  analogWrite(LED_RED, 0);
-  analogWrite(LED_BLUE, 0);
-  analogWrite(LED_GREEN, 0);
+  analogWrite(LED_RED, 255);
+  analogWrite(LED_BLUE, 255);
+  analogWrite(LED_GREEN, 255);
   
   Serial.begin(9600);
 }
@@ -26,29 +26,29 @@ void loop() {
     
     if (c == 'g') { // Jenkinsビルド開始
       Serial.println("START");
-      analogWrite(LED_RED, 0);
-      analogWrite(LED_BLUE, 1023); // 青LEDを点灯
-      analogWrite(LED_GREEN, 0);
+      analogWrite(LED_RED, 255);
+      analogWrite(LED_BLUE, 0); // 青LEDを点灯
+      analogWrite(LED_GREEN, 255);
     } else if (c == 'e') { // エラー
       Serial.println("ERROR");
-      analogWrite(LED_RED, 1023);  // 赤LEDを点灯
-      analogWrite(LED_BLUE, 0);
-      analogWrite(LED_GREEN, 0);
+      analogWrite(LED_RED, 0);  // 赤LEDを点灯
+      analogWrite(LED_BLUE, 255);
+      analogWrite(LED_GREEN, 255);
     } else if ( c == 's') { // 成功
       Serial.println("SUCCESS");
-      analogWrite(LED_RED, 0);
-      analogWrite(LED_BLUE, 0);
-      analogWrite(LED_GREEN, 1023);  // 緑LEDを点灯
+      analogWrite(LED_RED, 255);
+      analogWrite(LED_BLUE, 255);
+      analogWrite(LED_GREEN, 0);  // 緑LEDを点灯
     } else if (c =='r') {
       Serial.println("RESET");
-      analogWrite(LED_RED, 0);
-      analogWrite(LED_BLUE, 0);
-      analogWrite(LED_GREEN, 0);
+      analogWrite(LED_RED, 255);
+      analogWrite(LED_BLUE, 255);
+      analogWrite(LED_GREEN, 255);
     } else {
-      Serial.println("RESET");
-      analogWrite(LED_RED, 0);
-      analogWrite(LED_BLUE, 1023);
-      analogWrite(LED_GREEN, 1023);
+      Serial.println("etc...");
+      analogWrite(LED_RED, 255);
+      analogWrite(LED_BLUE, 255);
+      analogWrite(LED_GREEN, 255);
     }
   }
   
--- status.sh
+++ status.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-curl http://localhost:3000/status?status=$1
+RESULT=`curl http://gitlab.freebit.net:9090/jenkins/job/SilkBrowser/lastBuild/api/xml | perl -le '$_=<>;print [/<result>(.+?)</]->[0]'`
 
+echo $RESULT
+
+if [ $RESULT = SUCCESS ]
+then
+    echo "success"
+    curl http://localhost:3000/status?status=s
+else
+    echo "error"
+    curl http://localhost:3000/status?status=e
+fi
